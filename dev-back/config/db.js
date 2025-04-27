aaa9e570e7edf10dccfb031bda182ce6671adc7b
Selim Abouleila <115533108+Selim-Abouleila@users.noreply.github.com>
2025-04-27 20:16:12 +0200

    Update db.js

--- dev-back/config/db.js
+++ dev-back/config/db.js
@@ -2,13 +2,8 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // <- 👈 Ajout essentiel
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS, // <- attention : dans .env c'est DB_PASS, pas DB_PASSWORD
-  database: process.env.DB_NAME
-});
+
+const db = mysql.createConnection(process.env.Connect);
 
 db.connect((err) => {
   if (err) {
